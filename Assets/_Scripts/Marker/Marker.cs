05b09aead41259cd1aebf5a8da419bff8a125597
Frenqy <411937015@qq.com>
2019-09-20 18:06:21 +0800

    搭建marker功能的框架

--- Assets/_Scripts/Marker/Marker.cs
+++ Assets/_Scripts/Marker/Marker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -11,23 +12,85 @@ public class Marker : MonoBehaviour
     [SerializeField] private Transform[] MediaParent;
 
     [HideInInspector] public List<MediaSetting> medias;
+    [HideInInspector] public GameObject TextPrefab;
+    [HideInInspector] public GameObject ImagePrefab;
+    [HideInInspector] public GameObject VideoPrefab;
 
     public IEnumerator Init()
     {
-        foreach (var media in medias)
+       
+        //遍历当前marker的Media列表
+        for (int i = 0; i < medias.Count; i++)
         {
-            for (int i = 0; i < media.mediaPath.Length; i++)
+            //设置每个按钮的缩略图
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(@"file://" + medias[i].previewPath))
             {
-                using (UnityWebRequest webRequest = UnityWebRequest.Get(@"file://" + media.previewPath)) 
-                {
-                    webRequest.downloadHandler = new DownloadHandlerTexture();
-                    yield return webRequest.SendWebRequest();
-                    PreviewImg[i].texture = DownloadHandlerTexture.GetContent(webRequest);
-                }
-                using (UnityWebRequest webRequest = UnityWebRequest.Get(@"file://" + media.mediaPath[i]))
-                {
+                webRequest.downloadHandler = new DownloadHandlerTexture();
+                yield return webRequest.SendWebRequest();
+                PreviewImg[medias[i].buttonID].texture = DownloadHandlerTexture.GetContent(webRequest);
+            }
 
-                }
+            //设置按钮本身
+            buttons[medias[i].buttonID].gameObject.SetActive(true);
+            buttons[medias[i].buttonID].onClick.AddListener(() => MediaParent[i].gameObject.SetActive(MediaParent[i].gameObject.activeSelf));
+
+            //根据媒体类型将媒体加载进来
+            switch (medias[i].mediaType)
+            {
+                case MediaType.None:
+                    break;
+                case MediaType.Text:
+                    yield return StartCoroutine(InitText(i));
+                    break;
+                case MediaType.Image:
+                    yield return StartCoroutine(InitImage(i));
+                    break;
+                case MediaType.Video:
+                    yield return StartCoroutine(InitVideo(i));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    public IEnumerator InitText(int mediaIndex)
+    {
+        //为按钮添加媒体
+        for (int j = 0; j < medias[mediaIndex].mediaPath.Length; j++)
+        {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(@"file://" + medias[mediaIndex].mediaPath[j]))
+            {
+                yield return webRequest.SendWebRequest();
+                //UTF-8
+                string content = webRequest.downloadHandler.text;
+            }
+        }
+    }
+
+    public IEnumerator InitImage(int mediaIndex)
+    {
+        //为按钮添加媒体
+        for (int j = 0; j < medias[mediaIndex].mediaPath.Length; j++)
+        {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(@"file://" + medias[mediaIndex].mediaPath[j]))
+            {
+                webRequest.downloadHandler = new DownloadHandlerTexture();
+                yield return webRequest.SendWebRequest();
+                Texture2D content = DownloadHandlerTexture.GetContent(webRequest);
+            }
+        }
+    }
+
+    public IEnumerator InitVideo(int mediaIndex)
+    {
+        //为按钮添加媒体
+        for (int j = 0; j < medias[mediaIndex].mediaPath.Length; j++)
+        {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(@"file://" + medias[mediaIndex].mediaPath[j]))
+            {
+                yield return 0;
             }
         }
     }
---
+++ Assets/_Scripts/Marker/MarkerManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class MarkerManager : MonoBehaviour
+{
+    public GameObject MarkerPrefab;
+    public GameObject TextPrefab;
+    public GameObject ImagePrefab;
+    public GameObject VideoPrefab;
+
+    public Transform UIParent;
+
+    private List<Marker> markers = new List<Marker>();
+
+    public void Init(string jsonPath)
+    {
+        //读取设置
+        SettingManager.LoadSetting(jsonPath);
+
+        //提前生成marker（按Degrees数量而不是marker中配置数量生成，这样在管理状态的时候会更方便）
+        for (int i = 0; i < Triangel.Degrees.Length; i++)
+        {
+            GameObject go = Instantiate(MarkerPrefab, UIParent);
+            Marker m = go.GetComponent<Marker>();
+            markers.Add(m);
+        }
+
+        //将详细的媒体配置信息传到marker的脚本内
+        for (int i = 0; i < SettingManager.setting.markers.Count; i++)
+        {
+            int id = SettingManager.setting.markers[i].MarkerID;
+            markers[id].medias = SettingManager.setting.markers[i].medias;
+
+            markers[id].TextPrefab = TextPrefab;
+            markers[id].ImagePrefab = ImagePrefab;
+            markers[id].VideoPrefab = VideoPrefab;
+        }
+    }
+}

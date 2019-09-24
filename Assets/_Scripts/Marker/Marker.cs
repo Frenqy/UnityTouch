c59a93a75e04b116b4fa73316a3b880282981557
Frenqy <411937015@qq.com>
2019-09-24 18:02:37 +0800

    基础IMG功能

--- Assets/_Scripts/Marker/Marker.cs
+++ Assets/_Scripts/Marker/Marker.cs
@@ -38,7 +38,20 @@ public class Marker : MonoBehaviour
 
             //设置按钮本身
             buttons[medias[i].buttonID].transform.parent.gameObject.SetActive(true);
-            buttons[medias[i].buttonID].onClick.AddListener(() => MediaParent[i].gameObject.SetActive(MediaParent[i].gameObject.activeSelf));
+
+            //大坑：
+            //此处必须使用 temp 变量储存 i 再传入
+            //不能直接将循环变量 i 传入
+            //否则该方法内创建的循环变量 i 将一直留在内存无法释放
+            //导致->调用 onClick 时，执行 onClick 内部语句时会按 i 最后（循环结束后）的值来执行，而不是 AddListener 时刻的 i 的值来执行
+            //如果使用 temp 储存一次 i 的值
+            //则可以在触发 onClick 时，按 AddListener 时刻的 temp 的值来执行内部语句
+            int temp = i;
+            buttons[medias[i].buttonID].onClick.AddListener(() =>
+            {
+                Debug.Log(temp);
+                MediaParent[temp].gameObject.SetActive(!MediaParent[temp].gameObject.activeSelf);
+            });
 
             //根据媒体类型将媒体加载进来
             switch (medias[i].mediaType)
@@ -49,7 +62,7 @@ public class Marker : MonoBehaviour
                     yield return StartCoroutine(InitText(i));
                     break;
                 case MediaType.Image:
-                    //yield return StartCoroutine(InitImage(i));
+                    yield return StartCoroutine(InitImage(i));
                     break;
                 case MediaType.Video:
                     //yield return StartCoroutine(InitVideo(i));
@@ -63,16 +76,21 @@ public class Marker : MonoBehaviour
         gameObject.SetActive(false);
     }
 
+    private void SetMediaActive(int index)
+    {
+        MediaParent[index].gameObject.SetActive(!MediaParent[index].gameObject.activeSelf);
+    }
+
     public IEnumerator InitText(int mediaIndex)
     {
-        //为按钮添加媒体
+        //为按钮添加文字（UTF8)
         for (int j = 0; j < medias[mediaIndex].mediaPath.Length; j++)
         {
             using (UnityWebRequest webRequest = UnityWebRequest.Get(@"file://" + medias[mediaIndex].mediaPath[j]))
             {
                 yield return webRequest.SendWebRequest();
-                //UTF-8
                 string content = webRequest.downloadHandler.text;
+
                 GameObject text = Instantiate(TextPrefab, MediaParent[mediaIndex]);
                 text.GetComponent<Text>().text = content;
             }
@@ -81,7 +99,7 @@ public class Marker : MonoBehaviour
 
     public IEnumerator InitImage(int mediaIndex)
     {
-        //为按钮添加媒体
+        //为按钮添加图片
         for (int j = 0; j < medias[mediaIndex].mediaPath.Length; j++)
         {
             using (UnityWebRequest webRequest = UnityWebRequest.Get(@"file://" + medias[mediaIndex].mediaPath[j]))
@@ -89,6 +107,11 @@ public class Marker : MonoBehaviour
                 webRequest.downloadHandler = new DownloadHandlerTexture();
                 yield return webRequest.SendWebRequest();
                 Texture2D content = DownloadHandlerTexture.GetContent(webRequest);
+
+                GameObject img = Instantiate(ImagePrefab, MediaParent[mediaIndex]);
+                RawImage raw = img.GetComponent<RawImage>();
+                raw.texture = content;
+                raw.SetNativeSize();
             }
         }
     }

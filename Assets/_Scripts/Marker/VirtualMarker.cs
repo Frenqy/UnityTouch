da9c8bc6d72ec1d3f37b915dc47fcc64074661e1
Hooooyu <hoyuworking@163.com>
2019-10-31 11:51:49 +0800

    完成虚拟MK的展开和折叠功能

--- Assets/_Scripts/Marker/VirtualMarker.cs
+++ Assets/_Scripts/Marker/VirtualMarker.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 namespace VIC.Creator.Marker
 {
-
     /// <summary>
-    /// 虚拟Marker
+    /// 虚拟Marker功能
     /// </summary>
     public class VirtualMarker : MonoBehaviour
     {
@@ -15,27 +15,58 @@ namespace VIC.Creator.Marker
         [SerializeField] private ButtonActionGroup[] btnActions;
         [SerializeField] private Transform[] MediaParent;
 
+        public AnyButtonClick onAnyBtnExpand;
+        public AnyButtonClick onAnyBtnCollapse;
         public Text mkID;
+
+        private void OnEnable()
+        {
+            onAnyBtnExpand += ExpandAnyItem;
+            onAnyBtnCollapse += CollapseAnyItem;
+        }
+
+        private void OnDisable()
+        {
+            onAnyBtnExpand -= ExpandAnyItem;
+            onAnyBtnCollapse -= CollapseAnyItem;
+        }
+
+        private void Start()
+        {
+            for(int i=0; i<btnActions.Length; i++)
+            {
+                btnActions[i].Init(onAnyBtnExpand, onAnyBtnCollapse);
+                btnActions[i].mediaPos = MediaParent[i];
+            }
+        }
 
         public void SetMkInfo(int id)
         {
             mkID.text = id.ToString();
         }
 
-        /// <summary>
-        /// 点击了加号按钮
-        /// </summary>
-        public void OnClickAdd()
+        public void ExpandAnyItem(int index)
         {
-
+            Debug.LogError("展开 " + index + "层级");
+            VirtualMarkerManager.Instance.SetMediaPos(MediaParent[index]);
+            SetAllButtonsActivation(false);
+            VirtualMarkerManager.Instance.SetAddState(true);
         }
 
-        /// <summary>
-        /// 点击了减号按钮
-        /// </summary>
-        public void OnClickReduce()
+        public void CollapseAnyItem(int index)
         {
+            Debug.LogError("折叠 " + index + "层级");
 
+            SetAllButtonsActivation(true);
+            VirtualMarkerManager.Instance.SetAddState(false);
+        }
+
+        public void SetAllButtonsActivation(bool isActive)
+        {
+            for (int i = 0; i < btnActions.Length; i++)
+            {
+                btnActions[i].btn.interactable = isActive;
+            }
         }
 
     }
@@ -43,28 +74,60 @@ namespace VIC.Creator.Marker
     [System.Serializable]
     public class ButtonActionGroup
     {
+        // 当前添加Button设置
         public Button btn;
+        public int btnIndex;
         public Image previewImg;
+        public Transform mediaPos;
 
-        private bool isAdd=false;
+        private bool isExpanded = false;
+
         private ButtonSetting buttonSetting;
-        private int number;
+        private UIGradient gradient;
+        private AnyButtonClick onExpandDelegate;
+        private AnyButtonClick onCollapseDelegate;
 
-        public void Init()
+        public void Init(AnyButtonClick onExpand, AnyButtonClick onCollapse)
         {
-            btn.onClick.AddListener(OnAdd);
+            btn.onClick.AddListener(OnExpand);
+            gradient = previewImg.gameObject.GetComponent<UIGradient>();
+            gradient.enabled = false;
+
+            this.onExpandDelegate = onExpand;
+            this.onCollapseDelegate = onCollapse;
         }
 
-        private void OnAdd()
+        private void OnExpand()
         {
-            isAdd = true;
-            Debug.LogError("虚拟MK点击了一次 " + number);
-            number++;
+            isExpanded = true;
+            gradient.enabled = true;
+            onExpandDelegate(btnIndex);
+
+            btn.onClick.RemoveAllListeners();
+            btn.onClick.AddListener(OnCollapse);
+
+            btn.interactable = true;
         }
 
-        public void SaveToSetting()
+        public void OnCollapse()
+        {
+            if(isExpanded)
+            {
+                isExpanded = false;
+                gradient.enabled = false;
+                onCollapseDelegate(btnIndex);
+
+                btn.onClick.RemoveAllListeners();
+                btn.onClick.AddListener(OnExpand);
+            }
+        }
+
+        public void GetSettingInfo()
         {
 
         }
     }
+
+    public delegate void AnyButtonClick(int index);
+
 }
---
+++ Assets/_Scripts/Marker/VirtualMarkerManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VIC.Creator.Marker
+{
+
+    public class VirtualMarkerManager : MonoBehaviour
+    {
+        public static VirtualMarkerManager Instance;
+
+        private Transform mediaPos { get; set; }
+        private bool canAdd = false;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public void SetMediaPos(Transform pos)
+        {
+            mediaPos = pos;
+        }
+
+        public void SetAddState(bool anyExpand)
+        {
+            canAdd = anyExpand;
+        }
+
+        public Transform GetMediaPos()
+        {
+            return mediaPos;
+        }
+
+        public void AddIntoMarker()
+        {
+            if(canAdd)
+            {
+                Debug.LogError("添加了一个卡片");
+            }
+        }
+    }
+}
